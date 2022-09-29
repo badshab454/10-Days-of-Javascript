4dd6f29128adc666249a43777ba999fbceca894b
badshab454 <badshab454@gmail.com>
2022-09-29 22:35:10 +0600

    Arrow Function

--- DAY 5/Arrow Functions.js
+++ DAY 5/Arrow Functions.js
@@ -5,5 +5,15 @@
  * nums: An array of numbers.
  */
 function modifyArray(nums) {
-    
+    const func = nums.map(function(num){
+
+        if (num % 2 == 0)
+            {
+                return 2*num;
+            }
+        else {
+            return 3*num;
+        }
+    });
+    return func;
 }

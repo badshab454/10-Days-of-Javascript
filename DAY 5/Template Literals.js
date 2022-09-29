cad0ac16958c562b8a5fdde69d2ade785cf41f42
badshab454 <badshab454@gmail.com>
2022-09-29 21:50:26 +0600

    Template Literals

--- DAY 5/Template Literals.js
+++ DAY 5/Template Literals.js
@@ -8,5 +8,14 @@
  * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
  */
 function sides(literals, ...expressions) {
-    
+    var area = expressions[0];
+    var Parameter = expressions[1];
+
+    var s1 = (perimeter + Math.sqrt(perimeter*perimeter - (16 * area))) / 4;
+    //console.log("s1: " + s1);
+    var s2 = (perimeter - Math.sqrt(perimeter * perimeter - (16 * area))) / 4;
+    //console.log("s2: " + s2);
+    var array = [s1, s2];
+    array = array.sort(function (a,b) {return a-b;});
+    return array;
 }

cde6966111ccd7e7a8216bab4b5986fbe96de63a
badshab454 <badshab454@gmail.com>
2022-10-01 21:13:15 +0600

    Bitwise Operator

--- DAY 6/Bitwise Operators.js
+++ DAY 6/Bitwise Operators.js
@@ -1,10 +1,13 @@
-function getMaxLessThan(n, k) {
+function getMaxLessThanK(n, k) {
     let maximum = 0;
     let current = -1;
-    for (let i = 1; i < k; i++) {
-        for (let j = i + 1; j <= n; j++) {
+    for (let i = 1; i < k; i++) 
+    {
+        for (let j = i + 1; j <= n; j++) 
+        {
             current = i & j;
-            if (current < k && current > maximum) {
+            if (current < k && current > maximum) 
+            {
                 maximum = current;
             }
         }

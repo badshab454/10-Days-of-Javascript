78332d111669a7fe563057cd1873144911f438f1
badshab454 <badshab454@gmail.com>
2022-10-03 23:17:14 +0600

    Regular Expressions II

--- DAY 7/Regular Expressions II.js
+++ DAY 7/Regular Expressions II.js
@@ -1,10 +1,12 @@
 function regexVar() {
     /*
      * Declare a RegExp object variable named 're'
-     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.', 
+     * followed by one or more letters.
      */
     
-    var re = RegExp(/^([aeiou]).*\1$/);
+    var re = (/^(Mr\.|/Dr\.|Er\.|Ms\.|Mrs\.)\s?[a-z|A-Z]+$/);
+    
     /*
      * Do not remove the return statement
      */
